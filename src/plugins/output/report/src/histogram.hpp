459dfc20b7a753b94fa46e270c0028af17925848
Michal Sedlak <xsedla0v@stud.fit.vutbr.cz>
2019-03-12 19:09:56 +0100

    Report plugin: more functionality + some refactor
    * handling of option templates and template withdrawals
    * refresh time histogram for UDP sessions
    * refactored histogram struct for better usage
    * unique ptrs with deleters for ipx_session and fds_template

---
+++ src/plugins/output/report/src/histogram.hpp
@@ -0,0 +1,49 @@
+#pragma once
+#include <vector>
+#include <climits>
+
+struct Histogram {
+    struct value_s {
+        int from;
+        int to;
+        int count;
+    };
+
+    int from = 0;
+    int to = 0;
+    int bin_width = 0;
+    int length = 0;
+    std::vector<int> counts;
+
+    Histogram() {}
+
+    Histogram(int from, int to, int bin_width) : from(from), to(to), bin_width(bin_width)
+    {
+        length = (to - from) / bin_width + 2;
+        counts.resize(length, 0);
+    }
+
+    void
+    operator()(int value)
+    {
+        if (value < from) {
+            counts[0]++;
+        } else if (value >= to) {
+            counts[length - 1]++;
+        } else {
+            counts[(value - from) / bin_width + 1]++;
+        }
+    }
+
+    value_s operator[](int index)
+    {
+        if (index == 0) {
+            return {INT_MIN, from - 1, counts[0]};
+        } else if (index == length - 1) {
+            return {to, INT_MAX, counts[length - 1]};
+        } else {
+            int x = (index - 1) * bin_width + from;
+            return {x, x + bin_width - 1, counts[index]};
+        }
+    }
+};
